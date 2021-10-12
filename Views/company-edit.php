f5aa08c2fdab923d9b429f6b7293041886e7dcef
SebasRibeiro <69637516+SebasRibeiro@users.noreply.github.com>
2021-10-11 23:23:44 -0300

    campos autocompletados con info

--- Views/company-edit.php
+++ Views/company-edit.php
@@ -11,35 +11,35 @@
 <main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
-               <h2 class="mb-4">Editar <?php $company->getName(); ?></h2>
+               <h2 class="mb-4">Editar <?php echo $company->getName(); ?></h2>
                <form action="<?php echo FRONT_ROOT ?>Company/Add" method="post" class="bg-light-alpha p-5">
                     <div class="row">                         
                          <div class="col-lg-4">
                               <div class="form-group">
                                    <label for="">Id</label>
-                                   <input type="text" name="recordId" value="" class="form-control">
+                                   <input type="text" name="recordId" value="<?php echo $company->getCompanyId(); ?>" class="form-control" readonly>
                               </div>
                          </div>
                          <div class="col-lg-4">
                               <div class="form-group">
                                    <label for="">Nombre</label>
-                                   <input type="text" name="name" value="" class="form-control">
+                                   <input type="text" name="name" value="<?php echo $company->getName(); ?>" class="form-control">
                               </div>
                          </div>
                          <div class="col-lg-4">
                               <div class="form-group">
                                    <label for="">Email</label>
-                                   <input type="email" name="email" value="" class="form-control">
+                                   <input type="email" name="email" value="<?php echo $company->getEmail(); ?>" class="form-control">
                               </div>
                          </div>
                          <div class="col-lg-4">
                               <div class="form-group">
                                    <label for="">PhoneNumber</label>
-                                   <input type="text" name="phoneNumber" value="" class="form-control">
+                                   <input type="text" name="phoneNumber" value="<?php echo $company->getPhoneNumber(); ?>" class="form-control">
                               </div>
                          </div>
                     </div>
-                    <button type="submit" class="btn btn-dark ml-auto d-block">Agregar</button>
+                    <button type="submit" class="btn btn-dark ml-auto d-block">Editar</button>
                </form>
           </div>
      </section>
