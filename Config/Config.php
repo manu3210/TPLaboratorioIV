c354ed374eae8ebac423e62e1fba1251c89d5619
SebasRibeiro <69637516+SebasRibeiro@users.noreply.github.com>
2021-10-29 12:15:56 -0300

    clases y archivos para coneccion a bdd

--- Config/Config.php
+++ Config/Config.php
@@ -1,11 +1,16 @@
 <?php namespace Config;
 
-define("ROOT", dirname(__DIR__) . "/");
-//Path to your project's root folder
-define("FRONT_ROOT", "/Labo4/TPLaboratorioIV/");
-define("VIEWS_PATH", "Views/");
-define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
-define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+    define("ROOT", dirname(__DIR__) . "/");
+    //Path to your project's root folder
+    define("FRONT_ROOT", "/Labo4/TPLaboratorioIV/");
+    define("VIEWS_PATH", "Views/");
+    define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
+    define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "labo4");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
 ?>
 
 
